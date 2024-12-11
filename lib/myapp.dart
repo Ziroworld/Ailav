32d73019433a6c8244c2e3dfb89541361ea06f90
meinkampf.369 <ziro0x0x0@gmail.com>
2024-12-11 13:57:33 +0545

    Added first screen and few resources.

---
+++ lib/myapp.dart
@@ -0,0 +1,18 @@
+import 'package:ailav/view/splash_screen_view.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
+      debugShowCheckedModeBanner: false,
+      title: "Ailav",
+      home: const SplashScreenView(),
+    );
+  }
+}
