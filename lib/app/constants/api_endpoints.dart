1ac54cb5cf178733a1ae892900ba6a3dcefbba2f
meinkampf.369 <ziro0x0x0@gmail.com>
2025-01-31 17:47:30 +0545

    added api endpoints

---
+++ lib/app/constants/api_endpoints.dart
@@ -0,0 +1,11 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const Duration connectionTimeout = Duration(seconds: 1000);
+  static const Duration receiveTimeout = Duration(seconds: 1000);
+  static const String baseUrl = "http://10.0.2.2:3000/api/v3/";
+
+  static const String login = "auth/login";
+  static const String register = "auth/register";
+  static const String uploadImage = "auth/uploadImage";
+}
