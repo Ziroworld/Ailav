e94417e598f729eb8f73fc0126ab96a0367ddd5c
meinkampf.369 <ziro0x0x0@gmail.com>
2025-02-27 20:57:08 +0545

    add additional project files and prefab configuration for x86, x86_64, arm64-v8a, and armeabi-v7a architectures; update pubspec.yaml with sensor dependencies

--- lib/app/myapp.dart
+++ lib/app/myapp.dart
@@ -2,6 +2,7 @@ import 'package:ailav/app/di/di.dart';
 import 'package:ailav/core/theme/get_application_theme.dart';
 import 'package:ailav/features/splash/presentation/view/splash_screen_view.dart';
 import 'package:ailav/features/splash/presentation/view_model/splash_cubit.dart';
+import 'package:ailav/sensor/near.detector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
         value: getIt<SplashCubit>(),
         child: const SplashScreenView(),
       ),
+      builder: (context, child) {
+        return GlobalProximityListener(child: child!);
+      },
     );
   }
 }
